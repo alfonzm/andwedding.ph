d874964e00a8028e5f96b954aa44ae674771e86c
AlfonzM <m.alfonz@gmail.com>
2023-06-18 14:15:48 +0800

    Add redirect links

---
+++ src/app/attire/page.js
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function Attire() {
+  redirect('https://drive.google.com/file/d/1QavAGKeL_Pg0ISI-csEhP3qNzV5oZJgy/view?usp=share_link')
+}
---
+++ src/app/rsvp/page.js
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function Attire() {
+  redirect('https://forms.gle/eQyRrN7P8495EynJ7')
+}
